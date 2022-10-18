e4e92e496b2bfa9e6354ef2b36700bc881a8a448
Rodrigo <rodrigo.facury14@gmail.com>
2022-10-18 17:50:56 -0300

    feat: aumenta a necessidade de refatoração

--- refactor.js
+++ refactor.js
@@ -5,23 +5,23 @@ function returnRandomNumber() {
 function sumToRandomNumber(num) {
   const numberToSum = returnRandomNumber();
 
-  return `Seu número é ${numberToSum + num}!`;
+  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
 }
 
 function subtractRandomNumber(num) {
   const numberToSubtract = returnRandomNumber();
 
-  return `Seu número é ${numberToSubtract - num}!`;
+  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
 }
 
 function multiplyToRandomNumber(num) {
   const numberToMultiply = returnRandomNumber();
 
-  return `Seu número é ${numberToMultiply * num}!`;
+  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
 }
 
 function divideRandomNumber(num) {
   const numberToDivide = returnRandomNumber();
 
-  return `Seu número é ${numberToDivide / num}!`;
+  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
 }
