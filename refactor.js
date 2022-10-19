2f0b571b099bbb5b6c1967e79b5e47f8a9c2fd0e
vithxrlorencetti <vithorlorencetti@gmail.com>
2022-10-18 21:46:32 -0300

    Refactor

--- refactor.js
+++ refactor.js
@@ -1,27 +1,12 @@
 function returnRandomNumber() {
-  return Math.random();
+  return Math.round(Math.random() * 100);
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
+const yourNumberIs = `Seu número é `;
 
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
-}
-
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
-
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
-
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+const calculate = {
+  sum: function(num) {return yourNumberIs + (returnRandomNumber() + num);},
+  substract: function(num) {return yourNumberIs + (returnRandomNumber() - num);},
+  multiply: function(num) {return yourNumberIs + (returnRandomNumber() * num);},
+  divide: function(num) {return yourNumberIs + (returnRandomNumber() / num);},
 }
